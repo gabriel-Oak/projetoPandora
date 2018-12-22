541a11bb0698951c68608dc98b201ea8799a4eab
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2018-12-22 15:48:07 -0200

    Updates

---
+++ precache-manifest.3477bb318b872ec9c5ed3d1893117bdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projetoPandora/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "744247de84e15979f611",
+    "url": "/projetoPandora/static/js/runtime~main.744247de.js"
+  },
+  {
+    "revision": "9a6d410ce23364935559",
+    "url": "/projetoPandora/static/js/main.9a6d410c.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/projetoPandora/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "9a6d410ce23364935559",
+    "url": "/projetoPandora/static/css/main.61b5123b.chunk.css"
+  },
+  {
+    "revision": "3710b11736882f8f800824d4534b9e32",
+    "url": "/projetoPandora/index.html"
+  }
+];
