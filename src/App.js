6c1971329dc2d7a6f2e48a6d98b6804863744ce9
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2018-12-24 03:27:14 -0200

    menus configurados

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="app">
         
         <input type="checkbox" id="check"/>
-        <label id="botaoM" for="check"><img src={process.env.PUBLIC_URL + "/img/menu-4x.png"} /></label>
+        <label id="botaoM" for="check"><img src={process.env.PUBLIC_URL + "/img/menu-4x.png"} alt="Menu" /></label>
         
 
         <Menu />
