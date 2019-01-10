fd11ab4ed3cc8880e87c96465aa76ec98724cc8d
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2019-01-09 23:09:49 -0200

    Updates

--- precache-manifest.9d492518daa94f384e6ecbb86ebfbc13.js
+++ precache-manifest.9d492518daa94f384e6ecbb86ebfbc13.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/projetoPandora/static/js/runtime~main.744247de.js"
   },
   {
-    "revision": "a10ffe5404b41b97fd49",
-    "url": "/projetoPandora/static/js/main.a10ffe54.chunk.js"
+    "revision": "15c8d1234ac8563fad04",
+    "url": "/projetoPandora/static/js/main.15c8d123.chunk.js"
   },
   {
-    "revision": "633c191f63029ecc1695",
-    "url": "/projetoPandora/static/js/1.633c191f.chunk.js"
+    "revision": "5c141fa93a5a5864c362",
+    "url": "/projetoPandora/static/js/1.5c141fa9.chunk.js"
   },
   {
-    "revision": "a10ffe5404b41b97fd49",
+    "revision": "15c8d1234ac8563fad04",
     "url": "/projetoPandora/static/css/main.cc953dff.chunk.css"
   },
   {
-    "revision": "633c191f63029ecc1695",
+    "revision": "5c141fa93a5a5864c362",
     "url": "/projetoPandora/static/css/1.fce5e71f.chunk.css"
   },
   {
-    "revision": "d2db793b048b0db67dc38b3a3869ec0f",
+    "revision": "02783ce12b4dd346fe1e686a8f80d57d",
     "url": "/projetoPandora/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projetoPandora/precache-manifest.171dd6163f3100da702a975ff5386690.js"
+  "/projetoPandora/precache-manifest.9d492518daa94f384e6ecbb86ebfbc13.js"
 );
 
 workbox.clientsClaim();
